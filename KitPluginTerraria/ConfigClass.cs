1179f910a20547faa3008700fbd1e86cd59ad71f
Алексей <@DESKTOP-D98N22D>
2023-03-18 18:35:31 +0300

    Добавьте файлы проекта.

---
+++ KitPluginTerraria/ConfigClass.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using ReLogic.Peripherals.RGB.Logitech;
+using System.Text.Json;
+using System.Xml;
+
+public class Kit
+{
+    public int Cooldown { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public List<Item>? Items { get; set; }
+
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public List<string>? PermissionGroups { get; set; }
+}
+
+public class Item
+{
+    public int NetID { get; set; }
+    public int Stack { get; set; }
+}
+
+public class Config
+{
+    public Dictionary<string, Kit>? Kits { get; set; }
+    public List<string>? PermissionsKitload { get; set; }
+
+    public static string pathConfig = "ConfigKits.json";
+    public static Config? config;
+
+    // Сохраняет конфиг
+    public static void SaveConfig()
+    {
+        string json = JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(pathConfig, json);
+    }
+    // Метод для загрузки конфига из JSON файла при инициализации
+    public static void ConfigIsNull()
+    {
+        Config.config = new Config
+        {
+            Kits = new Dictionary<string, Kit>(),
+            PermissionsKitload = new List<string> { "superadmin", "allperm" }
+        };
+        string json = JsonConvert.SerializeObject(config, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(pathConfig, json);
+    }
+    public static void LoadConfigWithMessages(CommandArgs args)
+    {
+        if (!File.Exists(pathConfig))
+        {
+            args.Player.SendErrorMessage($"[KitPlugin] Конфиг файл {pathConfig} не найден! Создание нового конфига...");
+            ConfigIsNull();
+            args.Player.SendInfoMessage($"[KitPlugin] Конфиг {pathConfig} успешно создан и загружен!");
+        }
+        else
+        {
+            string json = File.ReadAllText(pathConfig);
+            config = JsonConvert.DeserializeObject<Config>(json);
+            if (config == null)
+            {
+                ConfigIsNull();
+                args.Player.SendInfoMessage($"[KitPlugin] Конфиг загружен успешно!");
+            }
+        }
+
+        try
+        {
+            foreach (var kit in config.Kits.Values)
+            {
+                // Проверяем, что список предметов не является null
+                if (kit.Items == null)
+                {
+                    throw new Exception($"список предметов набора {kit} не найден.");
+                }
+            }
+
+            args.Player.SendInfoMessage($"[KitPlugin] Конфиг загружен успешно!");
+        }
+        catch (Exception e)
+        {
+            args.Player.SendErrorMessage($"[KitPlugin] Ошибка при загрузке конфига: {e.Message}");
+        }
+    }
+
+    public static void LoadConfig()
+    {
+        if (!File.Exists(pathConfig))
+        {
+            TShock.Log.ConsoleError($"[KitPlugin] Конфиг файл {pathConfig} не найден! Создание нового конфига...");
+            ConfigIsNull();
+            TShock.Log.ConsoleInfo($"[KitPlugin] Конфиг {pathConfig} успешно создан и загружен!");
+        }
+        else
+        {
+            string json = File.ReadAllText(pathConfig);
+            config = JsonConvert.DeserializeObject<Config>(json);
+            if (config == null)
+            {
+                ConfigIsNull();
+                TShock.Log.ConsoleInfo($"[KitPlugin] Конфиг загружен успешно!");
+            }
+        }
+
+        try
+        {
+            foreach (var kit in config.Kits.Values)
+            {
+                // Проверяем, что список предметов не является null
+                if (kit.Items == null)
+                {
+                    throw new Exception($"список предметов набора {kit} не найден.");
+                }
+            }
+
+            TShock.Log.ConsoleInfo($"[KitPlugin] Конфиг загружен успешно!");
+        }
+        catch (Exception e)
+        {
+            TShock.Log.ConsoleError($"[KitPlugin] Ошибка при загрузке конфига: {e.Message}");
+        }
+    }
+    public static void GameAddkit(CommandArgs args)
+    {
+        try
+        {
+            int netId = 0;
+            int stack = 0;
+            if (args.Parameters.Count == 4)
+            {
+                if (!int.TryParse(args.Parameters[2], out netId) || !int.TryParse(args.Parameters[3], out stack))
+                {
+                    args.Player.SendErrorMessage("Вы используете не верные параметры: /kit add <название кита> <netID> <stack> - netID и stack должны быть целыми числами, stack по умолчанию равен 1");
+                    return;
+                }
+            }
+            else if (args.Parameters.Count == 3)
+            {
+                stack = 1;
+                if (!int.TryParse(args.Parameters[2], out netId))
+                {
+                    args.Player.SendErrorMessage("Вы используете не верные параметры: /kit add <название кита> <netID> <stack> - netID и stack должны быть целыми числами, stack по умолчанию равен 1");
+                    return;
+                }
+            }
+            else if (args.Parameters.Count == 2)
+            {
+                netId = 0;
+                stack = 1;
+            }
+            string kitName = args.Parameters[1];
+            // Проверяем существует ли кит с указанным именем
+            if (!config.Kits.ContainsKey(kitName))
+            {
+                // Создаем новый кит
+                config.Kits[kitName] = new Kit
+                {
+                    Cooldown = 60 * 15, // Задаем значения по умолчанию 15 часов
+                    Items = new List<Item>(),
+                    PermissionGroups = new List<string>()
+                };
+            }
+
+            // Создаем новый предмет и добавляем его в список предметов кита
+            var newItem = new Item
+            {
+                NetID = netId,
+                Stack = stack
+            };
+            config.Kits[kitName].Items.Add(newItem);
+            // Сохраняем изменения в файл конфигурации
+            SaveConfig();
+            args.Player.SendSuccessMessage("Файл конфига успешно модернизирован!");
+        }
+        catch (Exception e)
+        {
+            args.Player.SendErrorMessage($"Что-то пошло не так, ошибка:{e}");
+        }
+    }
+    public static void GameDeleteKit(CommandArgs args)
+    {
+        if (args.Parameters.Count != 2)
+        {
+            args.Player.SendErrorMessage($"Использование: /kit del <название кита>, это удалит кит и тут же сохранит это в конфиг");
+            return;
+        }
+        string kitName = args.Parameters[1];
+        // Проверяем существует ли кит с указанным именем
+        if (config.Kits.ContainsKey(kitName))
+        {
+            // Удаляем кит из конфигурации
+            config.Kits.Remove(kitName);
+
+            // Сохраняем изменения в файл конфигурации
+            SaveConfig();
+
+            args.Player.SendSuccessMessage($"Кит {kitName} удален.");
+        }
+        else
+        {
+            args.Player.SendErrorMessage($"Кит {kitName} не найден.");
+        }
+    }
+    public static void GameDelItemConfig(CommandArgs args)
+    {
+        if (args.Parameters.Count != 3)
+        {
+            args.Player.SendErrorMessage("Неверное количество параметров. Использование: /kit delitem <название кита> <netID>");
+        }
+        // Получаем аргументы команды
+        string kitName = args.Parameters[1];
+
+        // Проверяем, было ли передано число в качестве второго аргумента, и преобразуем его из строки в число
+        if (!int.TryParse(args.Parameters[2], out int netId))
+        {
+            args.Player.SendErrorMessage("Неверный формат аргумента netID.");
+            return;
+        }
+
+        // Проверяем, существует ли кит с указанным именем
+        if (!config.Kits.ContainsKey(kitName))
+        {
+            args.Player.SendErrorMessage($"Кит '{kitName}' не существует.");
+            return;
+        }
+
+        // Ищем предмет с указанным netID в списке предметов кита
+        int indexToRemove = -1;
+        for (int i = 0; i < config.Kits[kitName].Items.Count; i++)
+        {
+            if (config.Kits[kitName].Items[i].NetID == netId)
+            {
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        // Если предмет не найден, выводим сообщение об ошибке
+        if (indexToRemove == -1)
+        {
+            args.Player.SendErrorMessage($"Предмет с netID {netId} не найден в ките '{kitName}'.");
+            return;
+        }
+
+        // Удаляем предмет из списка предметов кита
+        config.Kits[kitName].Items.RemoveAt(indexToRemove);
+
+        // Сохраняем изменения в файл конфигурации
+        SaveConfig();
+
+        // Отправляем сообщение об успешном удалении предмета
+        args.Player.SendSuccessMessage($"Предмет с netID {netId} успешно удален из кита '{kitName}'.");
+    }
+    public static void SetKitCooldown(CommandArgs args)
+    {
+        string kitName = args.Parameters[1];
+
+        // Проверяем существует ли кит с указанным именем
+        if (!config.Kits.ContainsKey(kitName))
+        {
+            args.Player.SendErrorMessage($"Кит с названием \"{kitName}\" не существует.");
+            return;
+        }
+
+        if (!int.TryParse(args.Parameters[2], out int cooldown))
+        {
+            args.Player.SendErrorMessage($"\"{args.Parameters[1]}\" не является числом.");
+            return;
+        }
+
+        config.Kits[kitName].Cooldown = cooldown;
+        SaveConfig();
+
+        args.Player.SendSuccessMessage($"Задержка для кита \"{kitName}\" установлена на {cooldown} минут.");
+    }
+    public static void ShowKitPermissionGroups(CommandArgs args)
+    {
+        string kitName = args.Parameters[1];
+        if (config.Kits.ContainsKey(kitName))
+        {
+            var kit = config.Kits[kitName];
+            var permissionGroups = kit.PermissionGroups;
+
+            if (permissionGroups.Count == 0)
+            {
+                args.Player.SendErrorMessage($"К киту '{kitName}' не добавлено ни одной группы.");
+                return;
+            }
+
+            var groupList = string.Join(", ", permissionGroups);
+            args.Player.SendSuccessMessage($"Группы, имеющие доступ к киту '{kitName}': {groupList}");
+        }
+        else
+        {
+            args.Player.SendErrorMessage($"Кит '{kitName}' не найден в конфигурации.");
+        }
+    }
+
+    public static void GameAddPermToKitCommand(CommandArgs args)
+    {
+        // Проверяем правильность использования команды
+        if (args.Parameters.Count != 3)
+        {
+            args.Player.SendErrorMessage("Использование: /kit addperm <название кита> <название группы>");
+            return;
+        }
+
+        string kitName = args.Parameters[1];
+        string groupName = args.Parameters[2];
+
+        // Проверяем существует ли кит с указанным именем
+        if (!config.Kits.ContainsKey(kitName))
+        {
+            args.Player.SendErrorMessage($"Кит \"{kitName}\" не найден.");
+            return;
+        }
+
+        // Добавляем группу в список групп, которым доступен кит
+        config.Kits[kitName].PermissionGroups.Add(groupName);
+
+        // Сохраняем изменения в файл конфигурации
+        SaveConfig();
+
+        args.Player.SendSuccessMessage($"Группа \"{groupName}\" добавлена в список групп, которым доступен кит \"{kitName}\".");
+    }
+    public static void GameDelPermToKitCommand(CommandArgs args)
+    {
+        if (args.Parameters.Count != 3)
+        {
+            args.Player.SendErrorMessage("Использование: /kit delperm <название кита> <название группы>");
+            return;
+        }
+
+        string kitName = args.Parameters[1];
+        string groupName = args.Parameters[2];
+
+        if (!config.Kits.ContainsKey(kitName))
+        {
+            args.Player.SendErrorMessage($"Кит '{kitName}' не существует.");
+            return;
+        }
+
+        if (!config.Kits[kitName].PermissionGroups.Contains(groupName))
+        {
+            args.Player.SendErrorMessage($"Группа '{groupName}' не была добавлена к киту '{kitName}'.");
+            return;
+        }
+
+        config.Kits[kitName].PermissionGroups.Remove(groupName);
+        SaveConfig();
+
+        args.Player.SendSuccessMessage($"Группа '{groupName}' удалена из списка групп кита '{kitName}'.");
+    }
+}
---
+++ KitPluginTerraria/KitsPlagin.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.ComponentModel;
+using ReLogic.Peripherals.RGB.Logitech;
+
+namespace KitPlugin
+{
+    [ApiVersion(2, 1)]
+    public class KitPlugin : TerrariaPlugin
+    {
+        public override string Name => "KitPlugin";
+        public override string Author => "Синий Ёж";
+        public override string Description => "Плагин на киты с настройкой через ConfigKits.json";
+        public override Version Version => new Version(1, 0, 0);
+
+        public Dictionary<string, DateTime> lastUsedKitDates = new Dictionary<string, DateTime>();
+        public int CountKit = 0;
+        public KitPlugin(Main game) : base(game)
+        {
+
+        }
+        public override void Initialize()
+        {
+            ServerApi.Hooks.WorldSave.Register(this, OnSaveUsedKitDates);
+            Commands.ChatCommands.Add(new Command(KitCommand, "kit")
+            {
+                HelpText = "Использование: /kit <имя> выдаёт соответствующий набор. /kit list выдаст список всех наборов"
+            });
+            Config.LoadConfig();
+            string jsonString = JsonConvert.SerializeObject(Config.config);
+            TShock.Log.ConsoleInfo(jsonString);
+        }
+
+        private void OnSaveUsedKitDates(WorldSaveEventArgs args)
+        {
+            if (CountKit > 0)
+            {
+                if (File.Exists("lastUsedKitDates.json"))
+                {
+                    lastUsedKitDates = JsonConvert.DeserializeObject<Dictionary<string, DateTime>>(File.ReadAllText("lastUsedKitDates.json"));
+                }
+                File.WriteAllText("lastUsedKitDates.json", JsonConvert.SerializeObject(lastUsedKitDates));
+                CountKit = 0;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                base.Dispose(disposing);
+            }
+        }
+        private void KitCommand(CommandArgs args)
+        {
+            bool ignoreCoolDown = false;
+            int playerIndex = args.Player.Index;
+            string groupName = TShock.Players[playerIndex].Group.Name;
+            if (args.Parameters.Count < 1) // Проверка наличия необходимых параметров команды
+            {
+                args.Player.SendErrorMessage("Использование: /kit <название набора>, /kit help, /kit list");
+                return;
+            }
+            if (Config.config.PermissionsKitload.Contains(groupName)) //есть ли право на загрузку и изменение кита
+            {
+                ignoreCoolDown = true;
+                args.Player.SendSuccessMessage("Кажется у вас есть права на редактирование конфига для KitPlagin, расширенная справка - /kit help");
+                if (args.Parameters[0] == "help")
+                {
+                    args.Player.SendSuccessMessage("/kit load - просто загружает текущий файл конфига ConfigKits.json, будьте осторожны если хотите редактировать его напрямую.");
+                    args.Player.SendSuccessMessage("/kit del <название кита> - полностью удаляет кит.");
+                    args.Player.SendSuccessMessage("/kit additem <название кита> <netID> <stack> - добавляет новый предмет в кит, если кита нет он будет создан. stack по умолчанию равен 1.");
+                    args.Player.SendSuccessMessage("/kit delitem <название кита> <netID> - удаляет предмет из кита.");
+                    args.Player.SendSuccessMessage("/kit setcd <название кита> <задержка использования> - устанавливает задержку в минутах для кита.");
+                    args.Player.SendSuccessMessage("/kit showperm <название кита> - показывает список групп которые могут получать этот кит.");
+                    args.Player.SendSuccessMessage("/kit addperm <название кита> <название группы> - добавляет новую группу в список тех кому можно брать кит, осторожно - чувствительно к регистру.");
+                    args.Player.SendSuccessMessage("/kit delperm <название кита> <название группы> - удаляет группу из списка тех кому можно брать кит, осторожно - чувствительно к регистру.");
+                    return;
+                }
+                if (args.Parameters[0] == "load")
+                {
+                    Config.LoadConfigWithMessages(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "additem")
+                {
+                    Config.GameAddkit(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "del")
+                {
+                    Config.GameDeleteKit(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "delitem")
+                {
+                    Config.GameDelItemConfig(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "setcd")
+                {
+                    Config.SetKitCooldown(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "showperm")
+                {
+                    Config.ShowKitPermissionGroups(args);
+                }
+                else if (args.Parameters[0] == "addperm")
+                {
+                    Config.GameAddPermToKitCommand(args);
+                    return;
+                }
+                else if (args.Parameters[0] == "delperm")
+                {
+                    Config.GameDelPermToKitCommand(args);
+                    return;
+                }
+            }
+            string kitName = args.Parameters[0];
+            if (kitName == "help")
+            {
+                args.Player.SendSuccessMessage($"Команда /kit нужна для выдачи наборов. Список наборов - /kit list");
+            }
+            if (kitName == "list")
+            {
+                string message = "Наборы: ";
+                var kitNames = Config.config.Kits.Keys.Select(namekit => $"/kit {namekit}");
+                message += kitNames.Any() ? string.Join(", ", kitNames) : "null";
+                args.Player.SendSuccessMessage($"{message}");
+                return;
+
+            }
+            if (!Config.config.Kits.ContainsKey(kitName)) // Проверка существования кита с указанным именем
+            {
+                args.Player.SendErrorMessage($"Кит с именем \"{kitName}\" не найден.");
+                return;
+            }
+            if (!Config.config.Kits[kitName].PermissionGroups.Contains(groupName)) // Проверка наличия прав для использования кита
+            {
+                args.Player.SendErrorMessage("У вас нет прав на использование этого кита.");
+                return;
+            }
+            string KeyLastDates = args.Player.Name + "-" + kitName;
+            if (lastUsedKitDates.ContainsKey(KeyLastDates) && !ignoreCoolDown)
+            {
+                TimeSpan timeSinceSave = DateTime.Now - lastUsedKitDates[KeyLastDates];
+                double minutes = Config.config.Kits[kitName].Cooldown - timeSinceSave.TotalMinutes;
+                if (minutes > 0)
+                {
+                    args.Player.SendErrorMessage($"Вы недавно уже брали этот кит! Ждите {minutes} минут");
+                    return;
+                }
+                else
+                {
+                    //lastUsedKitDates.Remove(KeyLastDates);
+                    lastUsedKitDates[args.Player.Name + "-" + kitName] = DateTime.Now;
+                }
+            }
+            // Выдача предметов из кита
+            GiveKitItems(args.Player, Config.config.Kits[kitName].Items);
+
+            args.Player.SendSuccessMessage($"Кит \"{kitName}\" успешно получен.");
+            CountKit++;
+        }
+
+        // Метод для выдачи предметов из кита
+        private void GiveKitItems(TSPlayer player, List<Item> items)
+        {
+            foreach (Item item in items)
+            {
+                player.GiveItem(item.NetID, item.Stack);
+            }
+        }
+
+    }
+}
